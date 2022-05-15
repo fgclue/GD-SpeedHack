6b3911a47c5d164c100cabca43279fcc7870992c
fgclue <schefferdaudtfelipe@gmail.com>
2022-05-15 15:15:50 -0300

    first commit

---
+++ hack.py
@@ -0,0 +1,37 @@
+# This has to be imported early to clear the screen.
+import os
+
+os.system('cls')
+#    GGGGGGGGGGGG   DDDDDDDDDD   
+#   GGGGGG          DDDDDDDDDDDD Geometry Dash
+#   GGGG            DDDD    DDDD Speedhack
+#   GGGG   GGGGGGG  DDDD    DDDD made in ρƴᴛʜоɴ 3
+#   GGGG      GGGG  DDDD    DDDD 
+#   GGGGGG    GGGG  DDDDDDDDDDDD
+#    GGGGGGGGGGGG   DDDDDDDDDD  
+
+# Imports
+import time
+
+banline1 = ' GGGGGGGGGGGG   DDDDDDDDDD  '
+banline2 = 'GGGGGG          DDDDDDDDDDDD Geometry Dash'
+banline3 = 'GGGG            DDDD    DDDD Speedhack'
+banline4 = 'GGGG   GGGGGGG  DDDD    DDDD'
+banline5 = 'GGGG      GGGG  DDDD    DDDD'
+banline6 = 'GGGGGG    GGGG  DDDDDDDDDDDD'
+banline7 = ' GGGGGGGGGGGG   DDDDDDDDDD  '
+
+print('\n')
+print(banline1)
+print(banline2)
+print(banline3)
+print(banline4)
+print(banline5)
+print(banline6)
+print(banline7)
+print('\n')
+
+print("Please make sure GD is running. If GD is not running, please restart.")
+time.sleep(2)
+
+os.system('cls')
